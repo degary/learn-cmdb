143fbfe4a8571f1a4e66247c4f301f595cded89f
邓辉 <denghui@peogoo.com>
2021-03-02 18:33:19 +0800

    add static

--- controllers/base/base.go
+++ controllers/base/base.go
@@ -1,7 +1,15 @@
 package base
 
-import "github.com/astaxie/beego"
+import (
+	"fmt"
+	"github.com/astaxie/beego"
+)
 
 type BaseController struct {
 	beego.Controller
+}
+
+func (c *BaseController) Prepare() {
+	fmt.Println("xsrf")
+	c.Data["xsrf_token"] = c.XSRFToken()
 }
